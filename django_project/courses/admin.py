6b1a126b7c5c02776556f91dadb8241d51b7c327
Mustafa Mert Divarcı <mertdiv.97@gmail.com>
2022-05-07 02:40:29 +0300

    admin.py degisti

--- django_project/courses/admin.py
+++ django_project/courses/admin.py
@@ -1,4 +1,10 @@
 from django.contrib import admin
 from .models import Course
 # Register your models here.
-admin.site.register(Course)
+
+@admin.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ('name','available')
+#Admin alanında name ve available alanı oluşturdum.
+    list_filter = ('available',)
+    search_fields= ('name','description')
