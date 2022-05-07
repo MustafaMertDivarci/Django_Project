ba185b2d715ca891dc1b001c034a6e30f177973e
Mustafa Mert Divarcı <mertdiv.97@gmail.com>
2022-05-08 01:35:11 +0300

    Courses.html bozuldu

---
+++ django_project/courses/urls.py
@@ -0,0 +1,15 @@
+# Bu sayfayı ben oluşturdum ana dosyalardan django_project/urls.py kopyala yapıştırır ile.
+
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    
+    # path kendi içerisinde iki parametre alır:
+    # path(route,view,opt(kısayol ismi)) route->yönü verir
+
+    path('', views.course_list, name="courses"),
+
+
+   
+]
--- django_project/courses/views.py
+++ django_project/courses/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+# Kursları dinamik olarak gösterebilimek için models' a ihtiyacım var
+from . models import Course
 
 # Create your views here.
+def course_list(request):
+
+    # ORM yapıları ile dinamik bir şekilde veritabanından bilgilerimizi alıyoruz.
+    courses = Course.objects.all().order_by('-date')
+
+    context= {
+        'courses':courses
+    }
+
+    return render(request, 'courses.html',context)
